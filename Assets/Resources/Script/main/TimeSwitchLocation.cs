362eee61cc521a5bf8d0a88523c4f637bde4fd5f
misaka10843 <misaka10843@outlook.jp>
2021-11-04 16:10:03 +0800

    更新根据时间进行外景替换(main场景)

---
+++ Assets/Resources/Script/main/TimeSwitchLocation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimeSwitchLocation : MonoBehaviour
+{
+    private int hour;
+    private int TimeKey;
+    public Sprite[] TextureList;
+
+[SerializeField]
+Image myImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hour = DateTime.Now.Hour;
+        if(6 <= hour && hour < 10){
+          TimeKey = 0;
+        }else if(10 <= hour && hour < 17){
+          TimeKey = 1;
+        }else if(17 <= hour && hour < 19){
+          TimeKey = 2;
+        }else if(19 <= hour && hour < 21){
+          TimeKey = 3;
+        }else if(21 <= hour && hour < 2){
+          TimeKey = 4;
+        }else{
+          TimeKey = 3;
+        }
+        myImage.sprite = TextureList[TimeKey];
+    }
+}
