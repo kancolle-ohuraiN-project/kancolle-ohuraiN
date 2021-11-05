be5a02b898df7798c93bc6785d64a3c848c9c9d8
misaka10843 <misaka10843@outlook.jp>
2021-11-05 15:18:44 +0800

    通知动画完成('表示'动画)

--- Assets/Resources/Script/main/NotificationAnimation.cs
+++ Assets/Resources/Script/main/NotificationAnimation.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NotificationAnimation : MonoBehaviour
+public class NotificationAnimation : MonoBehaviour 
 {
-    [Header("动画的组件")]
-    public GameObject gaminitor;//获取组件
-
-    [Header("是否已经打开")]
-    public int isopen = 0;
-    public void OnClick()//定义一个点击函数
-    {
-      if(isopen == 0){
-        Debug.Log("isClick");
-        gaminitor.GetComponent<Animator>().enabled = true;// 激活动画组件
-        isopen = 1;
-      }else{
-        Debug.Log("isClick");
-        gaminitor.GetComponent<Animator>().enabled = false;// 激活动画组件
-      }
-        
-    }
+	[Header("动画的组件")]
+	public GameObject gaminitor;
+	[Header("是否已经打开")]
+	public int isopen = 0;
+	Animator m_Animator;
+	void Start() 
+	{
+		m_Animator = gameObject.GetComponent<Animator>();
+		m_Animator.SetBool("isclose", false);
+	}
+	public void OnClick()
+	{
+		if(isopen == 0) 
+		{
+			Debug.Log("isClick");
+      m_Animator.SetBool("isclose", false);
+			gaminitor.GetComponent<Animator>().enabled = true;
+			isopen = 1;
+		} else 
+		{
+			Debug.Log("isClick");
+      m_Animator.SetBool("isclose", true);
+			isopen = 0;
+		}
+	}
 }
