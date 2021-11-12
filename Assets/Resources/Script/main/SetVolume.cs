37ad46d204bfffaf9ab8f09f27ff3d8e46f948f8
misaka10843 <misaka10843@outlook.jp>
2021-11-12 15:04:05 +0800

    将语音文件具体分类，并且设置界面接入音量控制

---
+++ Assets/Resources/Script/main/SetVolume.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+ 
+public class SetVolume : MonoBehaviour
+{
+ 
+    public AudioMixer audioMixer;    // 进行控制的Mixer变量
+ 
+    public void SetMasterVolume(float volume)    // 控制主音量的函数
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+        // MasterVolume为我们暴露出来的Master的参数
+    }
+ 
+    public void SetBGMVolume(float volume)    // 控制背景音乐音量的函数
+    {
+        if(volume == -40f){
+          volume = -80f;
+        }
+        audioMixer.SetFloat("BGMVolume", volume);
+        // BGMVolume为我们暴露出来的BGM的参数
+    }
+
+    public void SetVoiceVolume(float volume)    // 控制音效音量的函数
+    {
+        audioMixer.SetFloat("VoiceVolume", volume);
+        // VoiceVolume为我们暴露出来的Voice的参数
+    }
+
+    public void SetSoundEffectVolume(float volume)    // 控制音效音量的函数
+    {
+        audioMixer.SetFloat("SoundEffectVolume", volume);
+        // SoundEffectVolume为我们暴露出来的SoundEffect的参数
+    }
+}
