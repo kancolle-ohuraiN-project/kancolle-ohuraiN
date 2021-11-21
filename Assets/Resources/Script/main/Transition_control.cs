8b2232023d0a28743070984d9176e8a1c5b04fa1
misaka10843 <misaka10843@outlook.jp>
2021-11-21 16:20:56 +0800

    增加主界面与战绩界面的切换

---
+++ Assets/Resources/Script/main/Transition_control.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class Transition_control : MonoBehaviour
+{
+  public GameObject A_Animator;
+  public GameObject startUI;
+  public GameObject endUI;
+
+  public void TransitionControl()
+  {
+    A_Animator.SetActive(true);
+    Invoke("TransitionControl1", 0.5f);
+  }
+  public void TransitionControl1()
+  {
+    startUI.SetActive(false);
+    endUI.SetActive(true);
+    Invoke("TransitionControl2", 0.5f);
+  }
+  public void TransitionControl2()
+  {
+    A_Animator.SetActive(false);
+  }
+}
