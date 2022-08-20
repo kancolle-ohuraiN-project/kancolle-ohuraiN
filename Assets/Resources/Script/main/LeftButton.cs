f694986bb0d228e64e42d739f72e2db9ef92b66d
misaka10843 <misaka10843@outlook.jp>
2022-08-20 17:42:32 +0900

    添加舰娘点击动画

--- Assets/Resources/Script/main/LeftButton.cs
+++ Assets/Resources/Script/main/LeftButton.cs
@@ -5,8 +5,10 @@ using System;
 using UnityEngine.UI;
 public class LeftButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [Tooltip("挂载时请关闭子组件image中的光线投射目标")]
     [Header("未悬停显示的对象")]
     public GameObject obj;
+    [Tooltip("挂载时请关闭子组件image中的光线投射目标")]
     [Header("悬停显示的对象")]
     public GameObject obj1;
     [Header("是否悬停")]
@@ -17,7 +19,6 @@ public class LeftButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         {
             obj.SetActive(false);
             obj1.SetActive(true);
-            Debug.LogWarning(isOn);
         }
         else
         {
--- Assets/Resources/Script/main/RandomKcSound.cs
+++ Assets/Resources/Script/main/RandomKcSound.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-public class RandomKcSound : MonoBehaviour 
+public class RandomKcSound : MonoBehaviour
 {
 	[Header("音频列表")]
 	public AudioClip[] audios;
+	public GameObject kansen;
 	// Start is called before the first frame update
-	public void RandomSound() 
+	public void RandomSound()
 	{
 		//获取音频数组数量
 		int n = 0;
-		for (int j = 0; j < audios.Length; j++) 
+		for (int j = 0; j < audios.Length; j++)
 		{
-			if (audios[j] != null) 
+			if (audios[j] != null)
 			{
 				n++;
 			}
 		}
 		//生成随机数
-		int  RandKey= Random.Range(0, n);
+		int RandKey = Random.Range(0, n);
 		//控制播放
 		this.GetComponent<AudioSource>().clip = audios[RandKey];
 		this.GetComponent<AudioSource>().Play();
-	}
+		//播放点击动画
+        kansen.GetComponent<Animator>().enabled = true;
+        kansen.GetComponent<Animator>().Play("舰娘点击",-1,0f);
+    }
 }
